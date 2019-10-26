10cf9f17250d861974796e6f6622ce25514c792b
fgwewegdvsd.fidelixbv <vinicius.fidellkcdklsix@GMEPN691254>
2019-10-26 11:11:22 -0300

    Enviar X e Y para o server

---
+++ Game/src/ConectarServer.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import javax.swing.JTextArea;
+
+public class ConectarServer implements Runnable{
+
+    String host;
+    int porta;
+    Thread threadPlayer;
+    Socket socket;
+    BufferedReader in;
+    PrintWriter out;
+    
+    public ConectarServer(String host, int porta){
+        this.host = host;
+        this.porta = porta;
+    }
+
+   public void configurarCliente(){
+        try {
+            socket = new Socket(host, porta);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+
+    public void receberMensagens(){
+        try {
+            String msg;
+            while((msg = in.readLine()) != null){
+                System.out.println(msg);
+                //TODO implementar os movimentos recebidos do adversário
+                // txtSaida.append(msg);
+                // txtSaida.append("\n");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void enviarMensagem(String msg){
+        out.println(msg);
+        out.flush();
+        System.out.println(msg);
+    }
+
+    public void iniciar(){
+        threadPlayer = new Thread(this);
+        threadPlayer.start();
+    }
+
+    @Override
+    public void run() {
+        configurarCliente();
+        receberMensagens();
+    }
+}
